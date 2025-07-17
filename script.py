211a099ea7fd72d568d8d2fd15f8b1e2dd31d38c
Tom Rehbein <t.rehbein@hirschtec.eu>
2025-07-18 00:26:48 +0200

    removing unused constans from secrets

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-from secrets import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REDIRECT_URI, TIDAL_USERNAME, TIDAL_PASSWORD
+from secrets import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REDIRECT_URI
 from InquirerPy import inquirer
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
